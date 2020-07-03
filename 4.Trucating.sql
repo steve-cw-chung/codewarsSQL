3ab32d32e2d29eec1d9cb3afb48fbba20368aa64
Steve Chung <steve.cw.chung@gmail.com>
2020-07-02 20:19:41 -0700

    truncating added

---
+++ 4.Trucating.sql
@@ -0,0 +1,9 @@
+/* Given the following table 'decimals':
+
+** decimals table schema **
+
+id
+number1
+number2
+Return a table with one column (towardzero) where the values are the result of number1 + number2 truncated towards zero. */
+SELECT TRUNC(number1 + number2) AS towardzero FROM decimals;
